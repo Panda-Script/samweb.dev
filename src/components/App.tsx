b3617780fc3440cc0c3a6e68d0bcc2cac1a48acd
Sam <104803895+Panda-Script@users.noreply.github.com>
2025-12-01 18:48:08 -0600

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,24 @@
 import "../styles.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
 import CardGrid from "./CardGrid";
 import { Intro } from "./Intro";
 import Profile from "./Profile";
-import { cardData } from "../data/cardData";
 import Header from "./Header";
+import NavigationBar from "./navbar";
 
-export default function App() {
+import NetworkSegLayout from "./netseg/NetworkSegLayout";
+
+// Your existing home page component
+function HomePage() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <h1 className="text-center my-4">My Projects</h1>
-      <CardGrid  />
+      <CardGrid />
       <Container>
         <Row>
           <Col sm={4}>
@@ -23,11 +26,62 @@ export default function App() {
           </Col>
           <Col sm={8}>
             <Intro />
-            
           </Col>
         </Row>
       </Container>
     </div>
   );
 }
+
+// New About page component
+function About() {
+  return (
+    <Container className="my-4">
+      <h1 className="text-center">About Me</h1>
+      <Row>
+        <Col>
+          <p>
+            This is the about page where you can share more information about
+            yourself.
+          </p>
+          {/* Add your about content here */}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+// New Contact page component
+function Contact() {
+  return (
+    <Container className="my-4">
+      <h1 className="text-center">Contact</h1>
+      <Row>
+        <Col md={6}>
+          <h3>Get in Touch</h3>
+          <p>Add your contact form or information here.</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <div style={{ paddingTop: "55px" }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/netseg/*" element={<NetworkSegLayout />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
 
